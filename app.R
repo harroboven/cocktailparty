82168ecb0637bad04612567afd522bc8386274ea
Uli Fretzen <ulifretzen@ma-145-5-203-242.mobile-devices.eur.nl>
2019-02-28 13:01:34 +0100

    sdf

--- app.R
+++ app.R
@@ -1,13 +1,23 @@
 # This is the main file of the Cocktail Shiny App :)
 
 library(shiny)
+library(markdown)
 # UI
-ui <- fluidPage( tabsetPanel(
+ui <- navbarPage(title = "Shiny Drinks",
   tabPanel("tab 1", "contents"),
-  tabPanel("tab 2", "contents"),
-  tabPanel("tab 3", "contents")))
+  navbarMenu("Networking Exploration",
+    tabPanel("Exploration by drinks", splitLayout(
+      actionButton(inputId, label, icon),
+      actionButton(inputId, label, icon)
+    ),
+    tabPanel("Exploration by ingredients", "contents"),
+    tabPanel("Bipartite visualization", "contents"))
+  ),
+  tabPanel("tab 3", "contents")
+)
 # Server
 server <- function(input, output) {
+  
 
 }
 
