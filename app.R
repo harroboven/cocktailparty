e01efd0f35f77b48e70e58df34f05f77b52fe851
harmanozan <harmanozan@yahoo.co.uk>
2019-02-28 14:58:08 +0100

    RadioButtons

--- app.R
+++ app.R
@@ -7,27 +7,36 @@ ui <- fluidPage(
   mainPanel(
     # testing code, remove later
     actionButton('showimage', 'Click me!', icon = NULL),
-    img(src = 'A.png', height = 140, width = 300)),
-  textInput("user.name.1", "The first name", ""),
-  textInput("user.name.2", "The second name", ""),
-  textInput("user.name.3", "The third name", ""),
-  textInput("user.name.4", "The fourth name", ""),
-  textOutput("salutation"),
+    img(src = 'A.png', height = 140, width = 300),
+    
+    # RadioButtons - distribution of obs.
+    radioButtons('dist.obs', 'Drinks distributed by:', 
+                 c('Alcoholic nature' = 'an',
+                   'Drink type' = 'dt',
+                   'Glass type' = 'gt',
+                   'Complexity' = 'cc',
+                   'Popularity' = 'pp',
+                   'Price' = 'pp')),
+    
+    
+    ),
   
   sidebarPanel()
-  
 )
 # Server
 server <- function(input, output) {
-  output$salutation <- renderText({
-    paste0("Hello ", input$user.name.1, ", ",
-    "Hello ", input$user.name.2, ", ",
-    "Hello ", input$user.name.3, ", ",
-    "Hello ", input$user.name.4, "! ")
-    })
-  # testing code wjltn
+
+  d <- reactive({
+    dist.obst <- switch (input$dist.obs,
+      an = action
+    )
+  })
+  # testing code 
   output$showimage <- renderImage({paste(img(src = 'B.png'))})
-}
+
+  
+  }
 
 #shinyApp()
 shinyApp(ui = ui, server = server)
+
