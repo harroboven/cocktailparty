07c1318534197ee243ea779eb6d8a9c9992a1e2b
Uli Fretzen <ulifretzen@ma-145-5-203-242.mobile-devices.eur.nl>
2019-02-28 14:26:27 +0100

    afsd

--- app.R
+++ app.R
@@ -6,18 +6,22 @@ library(markdown)
 ui <- navbarPage(title = "Shiny Drinks",
   tabPanel("tab 1", "contents"),
   navbarMenu("Networking Exploration",
-    tabPanel("Exploration by drinks", splitLayout(
-      actionButton(inputId, label, icon),
-      actionButton(inputId, label, icon)
-    ),
+    tabPanel("Exploration by drinks", 
+             verticalLayout(
+               titlePanel("Exploration by drinks"),
+               (splitLayout(
+                 verticalLayout(
+                   titlePanel("Summary Statistics of the Network by DRINKS"), 
+                   img(src = 'A.png', height = 300, width = 300)),
+                 titlePanel("Table with summary statistics"))))),
     tabPanel("Exploration by ingredients", "contents"),
-    tabPanel("Bipartite visualization", "contents"))
+    tabPanel("Bipartite visualization", "contents")
   ),
   tabPanel("tab 3", "contents")
 )
 # Server
 server <- function(input, output) {
-  output$showimage <- renderImage({img(src = 'B.png')}
+  output$showimage <- renderImage({img(src = 'B.png')})
 
 }
 
