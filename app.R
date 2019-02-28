76bdf316dc2e1297a248fa728fa0a54745fc7b93
harmanozan <harmanozan@yahoo.co.uk>
2019-02-28 19:56:31 +0100

    graphs added

--- app.R
+++ app.R
@@ -7,99 +7,41 @@ ui <- fluidPage(
   titlePanel("Hello boys! Your group seems to be awesome but I need your names!"),
   mainPanel(
     
-    # RadioButtons - distribution of obs.
-    radioButtons('dist.obs', 'Drinks distributed by:', 
-                 c('Alcoholic nature' = 'an',
-                   'Drink type' = 'dt',
-                   'Glass type' = 'gt',
-                   'Complexity' = 'cc',
-                   'Popularity' = 'pp',
-                   'Price' = 'pp'))
+  # SelectInput (Dropdown) - Network of one drink
+    all.drinks <- drinks[, .(name = unique(name), type = TRUE)],
+
+    all.ingredients <- drinks[, .(name = unique(ingredient), type = FALSE)],
+    all.vertices <- rbind(all.drinks, all.ingredients),
     
-    
-    
-    ),
-  # SliderInput - Network of drinks
-  sliderInput('weight.edges',
-              label = 'Min. weight of edges:', 
-              min = 1, max = 20, value = c(1,20), step = 1
-  ),
-  
-  # SelectInput (Dropdown) - Network of one drink 
-  selectInput('d',
-              label = 'Choose drink',
-              choices = c('',
-                          ''),
-              selected = 'Other'),
-  plotOutput(),
-  
-  # Title and tabpanels with drop-downs
-  navbarPage(title = "Shiny Drinks",
-            # 1st Drop-down tabpanels
-             navbarMenu("Data Desription", 
-                            tabPanel("Summary of Data",   
-                                     splitLayout( 
-                                       # object 1
-                                       verticalLayout( 
-                                         # object 1
-                                         titlePanel("Header Object 1"),
-                                         # object 2,
-                                         img(src ='A.png', height = 140, width =300)
-                                         ),
-                                       # object 2
-                                       verticalLayout( 
-                                         # object 1
-                                         titlePanel("Header Object 2"),
-                                         # Alcoholic nature histogram
-                                         plotOutput(outputId = "hist.alc.nat")
-                                         )
-                                       )
-                                     ),
-                        tabPanel("Data by Drinks", "content 2")
-                        ),
-            # 2nd tabpanel
-            navbarMenu("Networking Exploration",
-                       tabPanel("Exploration by drinks", 
-                                verticalLayout(
-                                  titlePanel("Exploration by drinks"),
-                                  (splitLayout(
-                                    verticalLayout(
-                                      titlePanel("Summary Statistics of the Network by DRINKS"), 
-                                      img(src = 'A.png', height = 300, width = 300)),
-                                    titlePanel("Table with summary statistics"))))),
-                       tabPanel("Exploration by ingredients", "contents"),
-                       tabPanel("Bipartite visualization", "contents")),
-            # 2nd tabpanel
-            tabPanel("tab 3", "contents")
+    g.drinks <- graph.data.frame(drinks[, .(name, ingredient)],
+                                 directed = FALSE,
+                                 vertices = all.vertices),
+    selectInput('d',
+              label = 'Choose a drink',
+              choices = drinks$name,
+              selected = NA)
   )
 
 )
+View(all.drinks)
+all.drinks <- drinks[, .(id = unique(name), type = TRUE)]
 
+all.ingredients <- drinks[, .(id = unique(ingredient), type = FALSE)]
+all.vertices <- rbind(all.drinks, all.ingredients)
+
+
+g.drinks.ingredients <- graph.data.frame(drinks[, .(name, ingredient)],
+                             directed = FALSE,
+                             vertices = all.vertices)
+g.drinks <- bipartite.projection(g.drinks.ingredients)$proj1
 
 # Server
 server <- function(input, output) {
-  
-  # histogram alcoholic nature
-  output$hist.alc.nat <- renderPlot({
-    ggplot(drinks[unique(id), ], aes(x = is_alcoholic)) + geom_bar()
-  })
-}
-
-  # RadioButtons - distr. of obs. 
-  d <- reactive({
-    dist.obst <- switch (input$dist.obs,
-      an = action
-    )
-  })
-  # SliderInput - Network of drinks 
-  output$scatterplot <- renderPlot({})
 
   # SelectInput (Dropdown) - Network of one drink 
+  reactivePlot()
   
-  
-  }
-
-
+}
 #shinyApp()
 shinyApp(ui = ui, server = server)
 
