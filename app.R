646229bcd65f1053ddb85786558d0a8424f3a6e6
Uli Fretzen <ulifretzen@ma-145-5-203-242.mobile-devices.eur.nl>
2019-02-28 10:49:57 +0100

    sd

--- app.R
+++ app.R
@@ -3,11 +3,12 @@
 library(shiny)
 # UI
 ui <- fluidPage(
-  
-  
-# IloveUli ....
-  # I hate Uli
-    # Forgive me, pls! <3
+  titlePanel("Hello boys! Your group seems to be awesome but I need your names!"),
+  textInput("user.name.1", "The first name", ""),
+  textInput("user.name.2", "The second name", ""),
+  textInput("user.name.3", "The third name", ""),
+  textInput("user.name.4", "The fourth name", ""),
+  textOutput("salutation")
 )
 # Server
 server <- function(input, output) {
