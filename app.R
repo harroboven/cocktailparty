26344475f7117982020eda9c47d357e378f1972e
FelixBIM <felix.meindl@gmail.com>
2019-02-28 13:24:03 +0100

    Yeah Baby! Do the committing

--- app.R
+++ app.R
@@ -2,10 +2,46 @@
 
 library(shiny)
 # UI
-ui <- fluidPage( tabsetPanel(
-  tabPanel("tab 1", "contents"),
-  tabPanel("tab 2", "contents"),
-  tabPanel("tab 3", "contents")))
+# Define UI for cocktail app ----
+ui <- fluidPage(
+  # Title and tabpanels with drop-downs
+  navbarPage(title = "Shiny Drinks",
+            # 1st Drop-down tabpanels
+             navbarMenu("Data Desription", 
+                            tabPanel("Summary of Data",   
+                                     splitLayout( 
+                                       # object 1
+                                       verticalLayout( 
+                                         # object 1
+                                         titlePanel("Header Object 1"),
+                                         # object 2,
+                                         img(src ='A.png', height = 140, width =300)
+                                         ),
+                                       # object 2
+                                       verticalLayout( 
+                                         # object 1
+                                         titlePanel("Header Object 2"),
+                                         # object 2,
+                                         img(src ='A.png', height = 140, width =300)
+                                         )
+                                       )
+                                     ),
+                        tabPanel("Data by Drinks", "content 2")
+                        ),
+            # 2nd tabpanel
+            tabPanel("tab 2", "contents"),
+            # 2nd tabpanel
+            tabPanel("tab 3", "contents")
+  )
+)
+
+
+
+  
+
+
+           
+
 # Server
 server <- function(input, output) {
 
