f7f4584d8accbad2fd4a1214471a66c152a4bac2
harroboven <harroboven@hotmail.com>
2019-02-28 10:53:54 +0100

    Server for hello example

--- app.R
+++ app.R
@@ -8,7 +8,12 @@ ui <- fluidPage(
 print("Zanis is not so cool")
 # Server
 server <- function(input, output) {
-  
+  output$salutation <- renderText({
+    paste0("Hello ", input$user.name.1, ", ",
+    "Hello ", input$user.name.2, ", ",
+    "Hello ", input$user.name.3, ", ",
+    "Hello ", input$user.name.4, "! ")
+    })
 }
 
 #shinyApp()
