775c2fc14f656036036e4140c92dbac67480d79d
FelixBIM <felix.meindl@gmail.com>
2019-02-28 15:17:53 +0100

    Added text feature; cleaned up code; improved layout

--- app.R
+++ app.R
@@ -8,43 +8,75 @@ ui <- fluidPage(
   navbarPage(title = "Shiny Drinks",
             # 1st Drop-down tabpanels
              navbarMenu("Data Desription", 
-                        tabPanel("Summary of Data",   
-                                 splitLayout( 
-                                   # left object 
-                                   verticalLayout( 
-                                     # object 1
-                                     titlePanel("Header Object 1"),
-                                     # object 2,
-                                     img(src ='A.png', height = 140, width =300)
-                                     ),
-                                   # right object 
-                                   verticalLayout( 
-                                     # object 1
-                                     titlePanel("Header Object 2"),
-                                     # object 2,
-                                     img(src ='A.png', height = 140, width =300)
+                        # 1st Drop-down item
+                        tabPanel("Summary of Data", 
+                                 verticalLayout(
+                                   # header of whole page
+                                   titlePanel("Summary of Data"),
+                                   # rest of page
+                                   splitLayout( 
+                                     # left object 
+                                     verticalLayout(
+                                       #title of left object
+                                       titlePanel("Explanation of Summary"),
+                                       #content of left object
+                                       p("INTROTEXT", 
+                                         style = "font-family: 'times'; font-si16pt")
+                                       ),
+                                     # right object 
+                                     verticalLayout( 
+                                       # object 1
+                                       titlePanel("Header Object 2"),
+                                       # object 2,
+                                       img(src ='A.png', height = 140, width =300)
+                                       )
                                      )
                                    )
-                                 ),
+                        ),
+                        # 2nd Drop-down item
                         tabPanel("Data by Drinks", "content 2")
                         ),
             # 2nd tabpanel
             navbarMenu("Networking Exploration",
+                       # 1st Drop-down item
                        tabPanel("Exploration by drinks", 
                                 verticalLayout(
+                                  # header of whole page
                                   titlePanel("Exploration by drinks"),
-                                  (splitLayout(
+                                  # rest of page
+                                  splitLayout(
+                                    # left object 
                                     verticalLayout(
-                                      titlePanel("Summary Statistics of the Network by DRINKS"), 
+                                      #title of left object
+                                      titlePanel("Summary Statistics of the Network by DRINKS"),
+                                      #content of left object
                                       img(src = 'A.png', height = 300, width = 300)),
-                                    titlePanel("Table with summary statistics"))))),
-                       tabPanel("Exploration by ingredients", "contents"),
-                       tabPanel("Bipartite visualization", "contents")),
+                                    # right object
+                                    verticalLayout(
+                                      #title of right object
+                                      titlePanel("Table with summary statistics"),
+                                      #content of right object
+                                      img(src = 'A.png', height = 300, width = 300)
+                                      )
+                                    )
+                                  )
+                                ),
+                       # 2nd Drop-down item
+                       tabPanel("Exploration by ingredients", 
+                                "contents"
+                                ),
+                       # 3rd Drop-down item
+                       tabPanel("Bipartite visualization", 
+                                "contents"
+                                )
+                       ),
             # 2nd tabpanel
-            tabPanel("tab 3", "contents")
+            tabPanel("tab 3", 
+                     "contents"
+                     )
             )
   )
-
+         
 
 # Server
 server <- function(input, output) {
