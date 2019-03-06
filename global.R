b864ba5331fe546e0e884d30602e9e6031218a90
FelixBIM <felix.meindl@gmail.com>
2019-03-06 16:49:28 +0100

    Added explanations

--- global.R
+++ global.R
@@ -2,7 +2,17 @@
 
 # Instruction that loads prepared data into memory
 dt.drinks <- readRDS("./data/longdrinks.rds")
+
+# Create column that states the complexity of the drink recipe
 dt.drinks <- dt.drinks[, complexity := str_count(dt.drinks$instructions)]
+
+# ???
 dt.drinks.filtered <- dt.drinks[unique(id), ]
+
+# ???
 l.is_alcoholic_values <- unique(dt.drinks$is_alcoholic)
+
+
+
+
 
