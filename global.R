f062651e7bb0a5388b4ea0c444e4d7a37839ea98
FelixBIM <felix.meindl@gmail.com>
2019-03-06 18:34:10 +0100

    Add: commonality of drinks

--- global.R
+++ global.R
@@ -7,6 +7,13 @@ dt.drinks <- dt.longdrinks
 # Add complexity of drink recipes
 dt.drinks <- dt.drinks[, complexity := str_count(dt.drinks$instructions)]
 
+# Add commonality of drinks
+dt.commonality <- read.csv("./data/common_drinks.csv")
+colnames(dt.commonality)[colnames(dt.commonality)=="x"] <- "name"
+dt.drinks <- merge(dt.drinks, dt.commonality[, c("name", "common_final")], by = "name", all.x = TRUE)
+colnames(dt.drinks)[colnames(dt.drinks)=="common_final"] <- "commonality"
+dt.drinks$commonality <- as.factor(dt.drinks$commonality)
+
 # Delete non-standardized columns from dt.drinks. They are still in dt.longdrinks, in case you need to access the original ones.
 dt.drinks[, c("amount", "category", "unit", "instructions", "glass_type") := NULL]
 
@@ -16,12 +23,11 @@ setnames(dt.drinks, old = c("std.unit", "std.glass", "std.category"), new = c("u
 # Delete thumbnail (not using it) and era (very few observations).
 dt.drinks[, c("thumbnail", "era") := NULL]
 
-# Making sure dt.drinks.filtered exhibits the changes as well
+# Making sure dt.drinks.filtered exhibits the changes as well //// just unique drinks????
 dt.drinks.filtered <- dt.drinks[unique(id), ]
 
 # ???
 l.is_alcoholic_values <- unique(dt.drinks$is_alcoholic)
-
 
 
 
